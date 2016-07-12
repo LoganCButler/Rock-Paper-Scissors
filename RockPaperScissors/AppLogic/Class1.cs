cc8af9e179073479cd0a4f89544f3a150e189d87
Logan Butler <logan.butler@agilethought.com>
2016-07-12 18:22:07 -0400

    Added basic logic for player one moves

---
+++ RockPaperScissors/AppLogic/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AppLogic
+{
+    public class Game
+    {
+        private int playerScore = 0;
+        private int computerScore = 0;
+
+        public void play(string playerMove, string computerMove)
+        {
+            switch (playerMove)
+            {
+                case "rock":
+                    if (computerMove == "rock")
+                    {
+                        break;
+                    }
+                    if (computerMove == "paper")
+                    {
+                        computerScore += 1;
+                        break;
+                    }
+                    if (computerMove == "scissors")
+                    {
+                        playerScore += 1;
+                        break;
+                    }
+                    break;
+            }
+        }
+
+        public string CheckForWin()
+        {
+            if(playerScore == 3)
+            {
+                return "Player One Wins!";
+            }
+            if (playerScore == 3)
+            {
+                return "Player One Wins";
+            }
+            return "";
+        }
+    }
+}
---
+++ RockPaperScissors/Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppLogic;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PlayerWinsWithRockRockRock()
+        {
+            //arrange
+            Game game = new Game();
+            game.play("rock", "scissors");
+            game.play("rock", "scissors");
+            game.play("rock", "scissors");
+
+            //act
+            string result = game.CheckForWin();
+
+            //assert
+            Assert.AreEqual("Player One Wins!", result);
+
+
+        }
+    }
+}
